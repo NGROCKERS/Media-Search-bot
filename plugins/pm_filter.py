51ea16d14cd0868da97ab516245700e749ebb8dd
NGROCKERS <77508122+NGROCKERS@users.noreply.github.com>
2021-09-08 09:31:46 +0400

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -329,8 +329,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{title}"
                 buttons = [
                     [
-                        InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122'),
-                        InlineKeyboardButton('Update Channel', url='https://t.me/subin_works')
+                        InlineKeyboardButton('More Bots', url='https://t.me/mallutalkies_bot'),
+                        InlineKeyboardButton('Update Channel', url='https://t.me/mallurockersclub')
                     ]
                     ]
                 
