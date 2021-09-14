6d343f0b5bd22febb694876d4eb596de3cd599dd
NGROCKERS <77508122+NGROCKERS@users.noreply.github.com>
2021-09-14 20:48:20 +0400

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -56,10 +56,10 @@ async def filter(client, message):
                 file_id = file.file_id
                 filename = f"[{get_size(file.file_size)}] {file.file_name}"
                 btn.append(
-                    [InlineKeyboardButton(text=f"{filename}",callback_data=fi" mallurockersclub#{file_id}")]
+                    [InlineKeyboardButton(text=f"{filename}",callback_data=f"mallurockersclub#{file_id}")]
                     )
         else:
-            await client.send_sticker(chat_id=message.from_user.id, sticker='C'" lIX1F1J2JvzZWHaicwNWbPs2tQI")
+            await client.send_sticker(chat_id=message.from_user.id, sticker='lIX1F1J2JvzZWHaicwNWbPs2tQI')
             return
 
         if not btn:
